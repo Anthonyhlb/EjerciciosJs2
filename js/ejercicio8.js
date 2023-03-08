b301ae5949d0e0aa657a78f573262f6372979307
Anthony <anthonyhildebrand20@gmail.com>
2023-03-08 06:05:22 -0300

    Se completo el octavo ejercicio

---
+++ js/ejercicio8.js
@@ -0,0 +1,11 @@
+let n = parseInt(prompt("Introduce un número entre 1 y 50: "));
+let str = "";
+for(let i=1; i<=n; i++) {
+  for(let j=1; j<=i; j++) {
+    str += j;
+  }
+  str += "\n" + "<br>";
+  
+}
+
+document.write(str);
